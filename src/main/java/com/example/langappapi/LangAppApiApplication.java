1ade2cebf758a3c89916559631480a381b988a0f
mulatusisay <mulatusisay1@gmail.com>
2021-05-10 14:42:19 +0300

    1st push from local repository

---
+++ src/main/java/com/example/langappapi/LangAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.langappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LangAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LangAppApiApplication.class, args);
+    }
+
+}
